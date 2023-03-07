1367524abda4382598f0c15d2226d4e476755c4d
DavidRose <DavidRose331@Gmail.com>
2023-03-06 21:57:28 -0600

    updated

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Text.Json.Nodes;
+
+namespace WeatherApp 
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var apiKey = "720ca1003cf1ae2521bdf0a398d29dd7";
+
+            Console.WriteLine("Enter in a city: ");
+            var cityName = Console.ReadLine();
+
+            var weatherMapURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+            string weatherResponse = client.GetStringAsync(weatherMapURL).Result;
+            JObject weatherObject = JObject.Parse(weatherResponse);
+
+            Console.WriteLine("Weather Object");
+            Console.WriteLine("Weather: ");
+            Console.WriteLine($"It is {weatherObject["main"]["temp"]} degrees Fahrenheit in {cityName}");
+
+
+        }
+    }
+}
+
+
+
